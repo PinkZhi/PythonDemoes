d2756c94420c485a4e7dd930d697013f5d0f6abd
zyw <zywoffice@163.com>
2020-01-19 14:48:32 +0800

    修改文件

--- Client/main.py
+++ Client/main.py
@@ -5,7 +5,7 @@ print("Start")
 
 try:
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    host = socket.gethostname()
+    host = "127.0.0.1"
     port = 9000
 
     s.connect((host, port))
